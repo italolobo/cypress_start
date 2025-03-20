591c60ff036e42e7c85070d9942dd83e8dad761d
Italo <italoantuneslobo@gmail.com>
2025-03-20 22:38:02 +0000

    aula 22

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -7,6 +7,15 @@ describe('template spec', () => {
     cy.wait(2000) // wait for 2 seconds
     cy.get('.product:visible').should('have.length', 4)
 
+    cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+
+    cy.get('.products').find('.product').each(($el, index, $list) => {
+      const textVeg = $el.find('h4.product-name').text()
+      if (textVeg.includes('Cashews')) {
+        cy.wrap($el).find('button').click()
+      }
+    })
+
 
   })
 
